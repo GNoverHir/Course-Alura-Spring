6e2761d85eede4bc7fedabda42e5ecc78befa922
Pedro Lucca <pedroluccaprofissional@gmail.com>
2024-08-20 15:43:28 -0300

    feat: adding get method

--- alura/src/main/java/spring/vollmed/alura/controller/MedicoController.java
+++ alura/src/main/java/spring/vollmed/alura/controller/MedicoController.java
@@ -4,10 +4,11 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import spring.vollmed.alura.cliente.DadosCadastroCliente;
+import spring.vollmed.alura.medico.*;
 import spring.vollmed.alura.medico.DadosCadastroMedico;
-import spring.vollmed.alura.medico.DadosCadastroMedico;
-import spring.vollmed.alura.medico.Medico;
-import spring.vollmed.alura.medico.MedicoRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medico")
@@ -20,6 +21,11 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> Listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
 }
